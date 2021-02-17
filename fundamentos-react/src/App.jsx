69e70083f60c790a6952b2a429b644fecf3232c9
kaellandrade <micael.santos@dcomp.ufs.br>
2021-02-17 15:56:47 -0300

    desafio

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
+import Aleatorio from './components/basicos/Aleatorio';
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
-import Aleatorio from './components/basicos/Aleatorio';
 
 export default _ => (  //com default podemos usar funções anônimas
     <div id="app">
         <h1>Fundamentos react</h1>
-        <Aleatorio titulo="Número aleatório" max={50} min={10}></Aleatorio>
+        <Aleatorio titulo="Número aleatório" min={10} max={50}></Aleatorio>
         <Fragmento></Fragmento>
         <ComParametro titulo="Situação do Aluno" aluno="Micael Andrade" nota={9.3}></ComParametro>
         <Primeiro></Primeiro>
--- fundamentos-react/src/components/basicos/Aleatorio.jsx
+++ fundamentos-react/src/components/basicos/Aleatorio.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
-export default (props) => {
-    let numero = Math.floor(Math.random() * (props.max + 1 - props.min) + props.min);
+export default props => {
+    const { min, max } = props; // usando destructuring com objetos
+    const numero = Math.floor(Math.random() * (max + 1 - min) + min);
     return (
         <React.Fragment>
             <h3>Número aleatório</h3>
-            <div id='painel'>
-                <h4>Seu número é: <strong>{numero}</strong></h4>
-            </div>
+            <p>
+                Número mínimo: {min}
+            </p>
+            <p>
+                Número máximo: {max}
+            </p>
+            <p>Seu número é: <strong>{numero}</strong></p>
+
         </React.Fragment>
     );
 }
