113a73fac20c29c31874b89da0c31622e980d81e
kaellandrade <micael.santos@dcomp.ufs.br>
2021-02-17 09:03:43 -0300

    usando componentes

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,11 +2,14 @@ import './index.css' // import realativo
 import ReactDOM from 'react-dom'
 import React from 'react'
 
+import Primeiro from './components/basicos/Primeiro';
 
 const element = document.getElementById('root');
+
 ReactDOM.render(
     <div>
-        <strong>Olá, React!</strong>
+        <Primeiro></Primeiro>
+
     </div>,
     element
 );
