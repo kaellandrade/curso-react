94811d7f11b62f6324146c1dcc0be8b0e36d3b27
kaellandrade <micael.santos@dcomp.ufs.br>
2021-02-17 09:13:45 -0300

    usando jsx para criar componentes

--- fundamentos-react/src/components/basicos/Primeiro.js
+++ fundamentos-react/src/components/basicos/Primeiro.js
@@ -1,3 +1,14 @@
+import React from 'react'
+
+/**
+ * Criando um primeiro componente usando a notação JSX
+ */
 export default function Primeiro() {
-    return 'Primeiro componente';
+    const msg = 'Seja bem vindo(a)!'
+    return (
+        <div>
+            <h2>Primeiro Componente</h2>
+            <p>{msg}</p> 
+        </div>
+    );
 }
