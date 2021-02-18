a1a9c945ee5b1d6fba59a668bc215ee70b1522b8
kaellandrade <micael.santos@dcomp.ufs.br>
2021-02-17 21:32:50 -0300

    usando props.childrem par adinamismo do conteudo HTML

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -4,13 +4,26 @@ import Aleatorio from './components/basicos/Aleatorio';
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
+import Card from './components/layout/Card'
 
 export default _ => (  //com default podemos usar funções anônimas
     <div id="app">
-        <h1>Fundamentos react</h1>
-        <Aleatorio titulo="Número aleatório" min={10} max={50}></Aleatorio>
-        <Fragmento></Fragmento>
-        <ComParametro titulo="Situação do Aluno" aluno="Micael Andrade" nota={9.3}></ComParametro>
-        <Primeiro></Primeiro>
+        <h1 className="Title">Fundamentos react</h1>
+        <Card titulo="#4 - Desafio Aleatório">
+            <Aleatorio titulo="Número aleatório" min={10} max={50}></Aleatorio>
+        </Card>
+
+        <Card titulo="#3 - Fragmento">
+            <Fragmento></Fragmento>
+        </Card>
+
+        <Card titulo="#2 - Usando Parametro">
+            <ComParametro titulo="Situação do Aluno" aluno="Micael Andrade" nota={9.3}></ComParametro>
+        </Card>
+
+        <Card titulo="#1 - Primeiro Componente">
+            <Primeiro></Primeiro>
+        </Card>
+
     </div>
 );
