11985adbc37bb881a9da9ed1821c0856558554a8
kaellandrade <micael.santos@dcomp.ufs.br>
2021-02-22 11:05:11 -0300

    Comunicação Indireta

--- fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
 export default props => {
+    const minIdade = 50;
+    const maxIdade = 70;
+    const gerarIdade = () => parseInt(Math.random() * (maxIdade - minIdade) + minIdade);
+    const gerarNerd = () => Math.random() > 0.5;
+    
     return (
         <div>
             <div>
                 <div>
                     Filho
                 </div>
-                <button onClick={e => { props.quandoClicar('João', 53, true) }
+                <button onClick={e => { props.quandoClicar('João', gerarIdade(), gerarNerd()) }
                 }>Fornecer Informações</button>
             </div>
         </div>
--- fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import IndiretaFilho from './IndiretaFilho'
 
 export default props => {
-    let nome = '?';
-    let idade = 0;
-    let nerd = false;
-
-    function fornecerInformacoes(nomeP, idadeP, nerdP) {
-        nome = nomeP;
-        idade = idadeP;
-        nerd = nerdP;
+    const [nome, setNome] = useState('?'); // retorna um array com duas posições primaira posição será o valor, segunda será uma função para alterar o valor
+    const [idade, setIdade] = useState(0); // retorna um array com duas posições primaira posição será o valor, segunda será uma função para alterar o valor
+    const [nerd, setNerd] = useState(false); // retorna um array com duas posições primaira posição será o valor, segunda será uma função para alterar o valor
+    console.log(useState('?'));
+    function fornecerInformacoes(nome, idade, nerd) {
+    setNome(nome);
+    setIdade(idade);
+    setNerd(nerd);
     }
 
 
@@ -17,7 +17,7 @@ export default props => {
         <div>
             <div>
                 <span>{nome} </span>
-                <span><strong>{idade}</strong></span>
+                <span><strong>{idade}</strong> </span>
                 <span>{nerd ? 'Verdade' : 'Falso'}</span>
             </div>
             <IndiretaFilho quandoClicar={fornecerInformacoes}></IndiretaFilho>
